83ccb425675e46a66ebd32545350bbccde8d8345
Jonathan Guyer <guyer@nist.gov>
2014-09-30 14:12:22 -0400

    Fix on Mac OS X 10.9 Mavericks
    Allow linker to find  crt1.10.5.o

--- openmpi/build.sh
+++ openmpi/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
+env LDFLAGS="-L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/lib/ $LDFLAGS" \
+  ./configure --prefix=$PREFIX
+#   CFLAGS="-mmacosx-version-min=10.9 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk $CFLAGS" \
+#   FFLAGS="-mmacosx-version-min=10.9 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk $FFLAGS"
 make
 make install
