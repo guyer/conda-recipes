6a0feab2279bd106baf8bfc6763ee7e0b0f4628c
Jonathan Guyer <guyer@nist.gov>
2016-01-01 16:50:19 -0500

    Enable all optional packages
    This deals with ML having undefined symbols, but I don't know why

--- trilinos/build.sh
+++ trilinos/build.sh
@@ -9,37 +9,38 @@ fi
 mkdir build
 cd build
 
+#     -D Trilinos_ENABLE_NOX:BOOL=ON \
+
+#     -D PYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER} \
+#     -D PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
+#     -D PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.${SO_EXT} \
+
+
+env LDFLAGS="-L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/lib/ $LDFLAGS" \
 cmake \
     -D CMAKE_BUILD_TYPE:STRING=RELEASE \
     -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
     -D DART_TESTING_TIMEOUT:STRING=600 \
     -D Trilinos_ENABLE_Fortran:BOOL=OFF \
-    -D Trilinos_ENABLE_TESTS:BOOL=OFF \
-    -D Trilinos_ENABLE_DEBUG:BOOL=OFF \
-    -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
-    -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF \
+    -D Trilinos_ENABLE_TESTS:BOOL=ON \
+    -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
     -D Trilinos_ENABLE_PyTrilinos:BOOL=ON \
-    -D Trilinos_ENABLE_Teuchos:BOOL=ON \
-    -D Trilinos_ENABLE_Epetra:BOOL=OFF \
-    -D Trilinos_ENABLE_Trutils:BOOL=OFF \
-    -D Trilinos_ENABLE_EpetraExt:BOOL=OFF \
-    -D Trilinos_ENABLE_Isorropia:BOOL=OFF \
-    -D Trilinos_ENABLE_AztecOO:BOOL=OFF \
-    -D Trilinos_ENABLE_Galeri:BOOL=OFF \
-    -D Trilinos_ENABLE_Amesos:BOOL=OFF \
-    -D Trilinos_ENABLE_Ifpack:BOOL=OFF \
-    -D Trilinos_ENABLE_Komplex:BOOL=OFF \
-    -D Trilinos_ENABLE_Anasazi:BOOL=OFF \
-    -D Trilinos_ENABLE_ML:BOOL=OFF \
-    -D Trilinos_ENABLE_NOX:BOOL=OFF \
-    -D Trilinos_ENABLE_Pliris:BOOL=OFF \
+    -D Trilinos_ENABLE_Epetra:BOOL=ON \
+    -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
+    -D Trilinos_ENABLE_AztecOO:BOOL=ON \
+    -D Trilinos_ENABLE_ML:BOOL=ON \
+    -D TPL_ENABLE_MPI:BOOL=ON \
+    -D MPI_BASE_DIR:PATH=${PREFIX} \
     -D BUILD_SHARED_LIBS:BOOL=ON \
     -D SWIG_EXECUTABLE:FILEPATH=$PREFIX/bin/swig \
+    -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D PYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER} \
     -D PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
     -D PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.${SO_EXT} \
-    -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -D PyTrilinos_INSTALL_PREFIX:PATH=$PREFIX \
     ..
 
-make
+make -j $CPU_COUNT
 make install
+
+
