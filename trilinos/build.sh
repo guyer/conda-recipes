c8b589e92c0ce36a1a5dccc56bfd169b1d1633fc
Jonathan Guyer <guyer@nist.gov>
2017-01-04 14:03:37 -0500

    Strip cruft
     * Restored STK
     * Removed obsolete comments
     * Removed LDFLAGS

--- trilinos/build.sh
+++ trilinos/build.sh
@@ -3,17 +3,7 @@
 mkdir build
 cd build
 
-#     -D Trilinos_ENABLE_NOX:BOOL=ON \
-
-#     -D PYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER} \
-#     -D PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
-#     -D PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}${SHLIB_EXT} \
-
-#         -D TPL_ENABLE_Boost=OFF \
-#    -D TPL_ENABLE_BoostLib=OFF \
-
-
-env LDFLAGS="-L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/lib/ $LDFLAGS" \
+# env LDFLAGS="-L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/lib/ $LDFLAGS" \
 cmake \
     -D CMAKE_BUILD_TYPE:STRING=RELEASE \
     -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
@@ -26,7 +16,6 @@ cmake \
     -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
     -D Trilinos_ENABLE_AztecOO:BOOL=ON \
     -D Trilinos_ENABLE_ML:BOOL=ON \
-    -D Trilinos_ENABLE_STK:BOOL=OFF \
     -D TPL_ENABLE_MPI:BOOL=ON \
     -D TPL_ENABLE_Netcdf=OFF \
     -D MPI_BASE_DIR:PATH=${PREFIX} \
